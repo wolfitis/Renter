d0ff5f5e0ec52d1dc0b6318c593f9f09a1e42daf
wolfitis <webblog123@gmail.com>
2021-04-14 08:20:27 -0400

    added functions

--- src/controllers/listingController.js
+++ src/controllers/listingController.js
@@ -2,36 +2,37 @@ import { listings } from '../db'
 
 
 // create listing
-export const create = async () => {
+export const create = async (malikMakanId, city, type, address, facilities, rent, details, availability, contact, imgs, dbOptions, dbClient) => {
     try {
-
+        return await listings.create(malikMakanId, city, type, address, facilities, rent, details, availability, contact, imgs, dbOptions, dbClient)
     } catch (err) {
         throw err
     }
 }
 
 // read listing
-export const read = async () => {
+export const read = async (filter, dbOptions, dbClient) => {
     try {
 
+        return await listings.readOne(filter, dbOptions, dbClient)
     } catch (err) {
         throw err
     }
 }
 
 // modify listing
-export const modify = async () => {
+export const modify = async (filter, dataToModify, dbOptions, dbClient) => {
     try {
-
+        return await listings.modifyOne(filter, dataToModify, dbOptions, dbClient)
     } catch (err) {
         throw err
     }
 }
 
 // remove listing
-export const remove = async () => {
+export const remove = async (filter, dbOptions, dbClient) => {
     try {
-
+        return await listings.remove(filter, dbOptions, dbClient)
     } catch (err) {
         throw err
     }
