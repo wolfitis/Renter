e39c23fd1bb93a47624ddafe54035362ee2afdd2
wolfitis <webblog123@gmail.com>
2021-04-14 03:19:09 -0400

    added listing controler - template

---
+++ src/controllers/listingController.js
@@ -0,0 +1,38 @@
+import { listings } from '../db'
+
+
+// create listing
+export const create = async () => {
+    try {
+
+    } catch (err) {
+        throw err
+    }
+}
+
+// read listing
+export const read = async () => {
+    try {
+
+    } catch (err) {
+        throw err
+    }
+}
+
+// modify listing
+export const modify = async () => {
+    try {
+
+    } catch (err) {
+        throw err
+    }
+}
+
+// remove listing
+export const remove = async () => {
+    try {
+
+    } catch (err) {
+        throw err
+    }
+}
