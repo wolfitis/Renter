1853792c65cfb1cd240a18bdedbe4fd054bbf8a4
wolfitis <webblog123@gmail.com>
2021-04-14 03:19:36 -0400

    added listing route - template

---
+++ src/routes/listingRoute.js
@@ -0,0 +1,14 @@
+import { json } from 'body-parser'
+
+const listingController = require('../controllers/listingController')
+
+export const createListing = async (req, res) => {
+    try {
+
+
+    } catch (err) {
+        return res.json({
+            err
+        })
+    }
+}
