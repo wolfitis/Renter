91c6bdcb55c6a0eda6514eeafc63c47275f5d1b8
wolfitis <webblog123@gmail.com>
2021-04-23 13:42:14 -0400

    update -

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,10 @@
 const listingRoutes = require('./listingRoute').routes
 
-export const routes = app => {
+// export const routes = app => {
+//     listingRoutes(app)
+// }
+
+export const routes = function (app) {
     listingRoutes(app)
+
 }
